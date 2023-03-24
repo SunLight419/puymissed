78655fb1e020b630dde6f530e2173512277708d8
sunlight419 <mintialol72@gmail.com>
2023-03-24 20:38:44 +0900

    mminor fix

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 const Header = () => {
   return (
     <header className="header header-margin-bottom">
-      <h1 className="header-title">MNTぷよぷよ連鎖シミュレーター with ChatGPT β</h1>
+      <h1 className="header-title">ぷよぷよ連鎖シミュレーター β</h1>
       <a href="https://twitter.com/kyopro_mint" target="_blank" rel="noopener noreferrer">
         <FontAwesomeIcon icon={faTwitter} size="2x" style={{ color: '#1da1f2' }} className="twitter-logo" />
       </a>
