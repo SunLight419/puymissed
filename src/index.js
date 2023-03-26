89cbad6587dcfa56d21771e2f4d61a04d4008161
sunlight419 <mintialol72@gmail.com>
2023-03-26 20:58:13 +0900

    fix google analytycs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ReactGA from 'react-ga'; // for google analytics
+
+// for google analytics
+ReactGA.initialize('YOUR_TRACKING_ID');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
